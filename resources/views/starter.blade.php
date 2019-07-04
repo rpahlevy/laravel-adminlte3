4af28521979aa48c265ad66a8f3e07f33ef1e8a9
rpahlevy <rio.pahlevy@gmail.com>
2019-07-04 08:26:58 +0700

    update template

--- resources/views/starter.blade.php
+++ resources/views/starter.blade.php
@@ -6,11 +6,12 @@
 	<div class="container-fluid">
 		<div class="row mb-2">
 			<div class="col-sm-6">
-				<h1 class="m-0 text-dark">Dashboard</h1>
+				<h1 class="m-0 text-dark">Starter</h1>
 			</div><!-- /.col -->
 			<div class="col-sm-6">
 				<ol class="breadcrumb float-sm-right">
-					<li class="breadcrumb-item active">Dashboard</li>
+                    <li class="breadcrumb-item" href="{{route('dashboard')}}">Dashboard</li>
+					<li class="breadcrumb-item active">Starter</li>
 				</ol>
 			</div><!-- /.col -->
 		</div><!-- /.row -->
